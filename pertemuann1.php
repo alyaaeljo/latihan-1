8a7490bca17f71fa7628193f87adf06f7a760204
alyaaelsiyana <55368395+alyaaeljo@users.noreply.github.com>
2019-09-22 13:40:50 +0700

    Add files via upload

---
+++ pertemuann1.php
@@ -0,0 +1,50 @@
+<?php
+
+//$x = 10;
+//$y = 5;
+
+//echo " Rumus luas persegi panjang adalah P x L";
+//echo "<br>" ;
+//echo "panjang =" , $x;
+//echo "<br>";
+//echo "lebar =", $y;
+//echo "<br>";
+//echo "Luas Persegi Panjang adalah = " . $x*$y;
+
+?>
+
+<?php
+$x = 20;
+$y = 2;
+$b = 12;
+$c = 8;
+$r = 50;
+$z = 1/2;
+$a = 3.14;
+
+echo "Rumus Luas Segitiga adalah 1/2 x a x t";
+echo "<br>";
+echo "alas =" , $x; 
+echo "<br>";
+echo "tinggi  =" , $y;
+echo "<br>";
+echo "Luas Segitiga adalah =" . $z*$x*$y;
+echo "<br>";
+echo "<br>";
+echo "Rumus Luas Lingkaran adalah phi x jari-jari (r x r)";
+echo "<br>";
+echo "phi = " , $a;
+echo "<br>";
+echo "jari-jari =" , $r;
+echo "<br>";
+echo "Luas Lingkaran adalah =" . $a*$r*$r; 
+echo "<br>";
+echo "<br>";
+echo "Rumus Luas Jajargenjang adalah alas X tinggi";
+echo "<br>";
+echo "alas =" , $b;
+echo "<br>";
+echo "tinggi =" , $c;
+echo "<br>";
+echo "Luas Jajargenjang adalah = " .$b * $c;
+?>
